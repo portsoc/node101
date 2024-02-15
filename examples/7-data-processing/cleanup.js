f9d68b010ddeac29f83e92c69aea0956fae391e4
Rich Boakes <rjb@port.ac.uk>
2024-02-15 14:43:28 +0000

    comments and output messages added

--- examples/7-data-processing/cleanup.js
+++ examples/7-data-processing/cleanup.js
@@ -1,10 +1,21 @@
-import { promises as fs } from 'fs';
-import { cleanup } from './util.js';
-
-if  (process.argv.length < 3) {
+/*
+ * Read a CSV file, and report on  whether there
+ * are duplicate entries in the first column.
+ *
+ * Optionally (if an output file is specified)
+ * perform a cleanup operation on the data where
+ * the last instance of a duplicate is kept, and
+ * writes the resulting data to a new CSV file.
+ * 
+ * e.g. node cleanup.js students.csv cleaned.csv
+ */
+if (process.argv.length < 3) {
   console.log('Usage: node cleanup <filename> [<output-filename>]');
   process.exit(1);
 }
+
+import { promises as fs } from 'fs';
+import { cleanup } from './util.js';
 
 const inputFile = process.argv[2];
 const outputFile = process.argv[3];
@@ -15,9 +26,13 @@ const data = await fs.readFile(inputFile,'utf-8');
 // process the data
 const processed = cleanup(data);
 
+// write processed data as a CSV file if required
 if  (outputFile) {
-  // write processed data as a CSV file
-  const asCSV = Object.values(processed).join('\n');
+  const asCSV = Object.values(processed.rows).join('\n');
   await fs.writeFile(outputFile, asCSV, 'utf-8');
-  console.log(`cleanup stored results in ${outputFile}`);
+  if (processed.removed > 0) {
+    console.log(`${processed.removed} duplicate${processed.removed===1?"":"s"} identified`);
+    console.log(`Size reduced from ${processed.before} to ${processed.after} rows`);
+  }
+  console.log(`Results written to ${outputFile}`);
 }
--- examples/7-data-processing/util.js
+++ examples/7-data-processing/util.js
@@ -2,16 +2,31 @@
  * Loop over every line in the CSV file and create an
  * object for each line, adding it to rows using the
  * first column as the key
+ * @param {string} data - The CSV data to process
+ * @returns {Object} - An object with the first column as the key
  */
 export function cleanup(data) {
-  const rows = {}
-  for (const row of data.split('\n')) {
+
+  // create an object to store the results
+  const result = {
+    rows: {},
+    before: 0,
+    after: 0,
+    removed:0
+  }
+  const rows = data.split('\n');
+  result.before = rows.length;
+
+  for (const row of rows) {
     const columns = row.split(',');
     const id = columns[0];
-    if (rows[id]) {
+    if (result.rows[id]) {
       console.log(`Duplicate ID: ${id}`);
+      result.removed++;
+    } else {
+      result.after++;
     }
-    rows[id] = columns;
+    result.rows[id] = columns;
   }
-  return rows;
+  return result;
 }
