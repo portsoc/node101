8dbc16932afcfa8f1b1d280d741d5a775263c578
Rich Boakes <rich@boakes.org>
2024-02-15 08:56:22 +0000

    :shirt:

--- dobbin/harness.js
+++ dobbin/harness.js
@@ -1,9 +1,9 @@
 // shortcuts for adding colour to console messages
-const off = "\x1b[0m";
-const red = msg => "\x1b[31m" + msg + off;
-const blue = msg => "\x1b[34m" + msg + off;
-const bgGreen = msg => "\x1b[42m" + msg + off;
-const bgYellow = msg => "\x1b[43m" + msg + off;
+const off = '\x1b[0m';
+const red = msg => '\x1b[31m' + msg + off;
+const blue = msg => '\x1b[34m' + msg + off;
+const bgGreen = msg => '\x1b[42m' + msg + off;
+const bgYellow = msg => '\x1b[43m' + msg + off;
 
 
 let count = 0;
@@ -12,9 +12,9 @@ export function test(name, fn) {
   count++;
   try {
     fn();
-    console.log(`[${count}] ${blue("Success")}: ${name}`);
+    console.log(`[${count}] ${blue('Success')}: ${name}`);
   } catch (e) {
-    console.error(`[${count}] ${red("Failure")}: ${name}`);
+    console.error(`[${count}] ${red('Failure')}: ${name}`);
     console.error(bgYellow(`[${count}] ${e.message}`));
     throw e;
   }
