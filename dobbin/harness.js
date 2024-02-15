e1e757fc42205a0315c064fa4299dd91869ddec0
Rich Boakes <rich@boakes.org>
2024-02-15 08:52:24 +0000

    remove chalk dep

--- dobbin/harness.js
+++ dobbin/harness.js
@@ -1,4 +1,10 @@
-import chalk from 'chalk';
+// shortcuts for adding colour to console messages
+const off = "\x1b[0m";
+const red = msg => "\x1b[31m" + msg + off;
+const blue = msg => "\x1b[34m" + msg + off;
+const bgGreen = msg => "\x1b[42m" + msg + off;
+const bgYellow = msg => "\x1b[43m" + msg + off;
+
 
 let count = 0;
 
@@ -6,14 +12,14 @@ export function test(name, fn) {
   count++;
   try {
     fn();
-    console.log(chalk.blue(`[${count}] Success: ${name}`));
+    console.log(`[${count}] ${blue("Success")}: ${name}`);
   } catch (e) {
-    console.error(chalk.red(`[${count}] Failure: ${name}`));
-    console.error(chalk.white.bgYellow.bold(`[${count}] ${e.message}`));
+    console.error(`[${count}] ${red("Failure")}: ${name}`);
+    console.error(bgYellow(`[${count}] ${e.message}`));
     throw e;
   }
 }
 
 export function msg(message) {
-  console.log(chalk.bgGreen.bold(` ${message} `));
+  console.log(bgGreen(` ${message} `));
 }
--- worksheet/utility.js
+++ worksheet/utility.js
@@ -1,5 +1,5 @@
 /**
- * Teh subtract function is complete and ready for use.
+ * The subtract function is complete and ready for use.
  */
 export function subtract(a,b) {
   return a-b;
@@ -11,49 +11,6 @@ export function subtract(a,b) {
  * "export" to the function definition (as seen in the
  * subtract example above)
  */
-export function add(a,b) {
+function add(a,b) {
   return a+b;
 }
-
-/**
- * Create a function called compare that accepts two arrays of numbers
- * and compares the contents.  It should return true if the arrays are
- * identical and false otherwise.
- *
- * i.e. you've done this before, so you're now reusing your own code
- * and turning it into a module.
- */
-export function compare(a,b) {
-  if (a.length !== b.length) return false;
-  for (let i = 0; i < a.length; i++) {
-    if (a[i] !== b[i]) return false;
-  }
-  return true;
-}
-
-// Create and export a function called zeroest that accepts an array of
-// integer numbers and returns the one closest to zero.  where a negative
-// and positive number are equally close to zero, the positive number
-// should be returned.
-export function zeroest(arr) {
-  let closest = arr[0];
-  for (let i = 1; i < arr.length; i++) {
-    if (Math.abs(arr[i]) < Math.abs(closest)) {
-      closest = arr[i];
-    }
-  }
-  return closest;
-}
-
-// Create and export a function called largest that accepts an array of
-// numbers and returns the largest.
-export function largest(arr) {
-  let largest = arr[0];
-  for (let i = 1; i < arr.length; i++) {
-    if (arr[i] > largest) {
-      largest = arr[i];
-    }
-  }
-  return largest;
-}
-
